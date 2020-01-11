d601cb8dba290000f2dcfd0bdd5fc20b6809ccd3
Jae-Son Song <jaesonsong@gmail.com>
2020-01-11 18:22:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,10 +45,10 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
+    end
+    
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else
-      false
     end
   end
 end
